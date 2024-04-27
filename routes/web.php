64214be277f578701747fba5eeecdfb6d5b2851b
Zaki <achmadikazum91@gmail.com>
2024-04-27 22:38:36 +0700

    first-init

---
+++ routes/web.php
@@ -0,0 +1,125 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AlternatifController;
+use App\Http\Controllers\KriteriaController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['prefix' => 'alternatif'], function () {
+
+    // weboender untuk alternatif
+    Route::group(['prefix' => 'weboender'], function () {
+        Route::get('/', [AlternatifController::class, 'index'])->name('alternatif.weboender.index')->middleware('auth');
+        Route::get('/create', [AlternatifController::class, 'create'])->name('alternatif.weboender.create')->middleware('auth');
+    });
+
+});
+
+Route::group(['prefix' => 'kriteria'], function () {
+
+    // weboender untuk kriteria
+    Route::group(['prefix' => 'weboender'], function () {
+        Route::get('/', [KriteriaController::class, 'index'])->name('kriteria.weboender.index')->middleware('auth');
+        Route::get('/create', [KriteriaController::class, 'create'])->name('kriteria.weboender.create')->middleware('auth');
+    });
+
+});
+
+
+
+
+
+
+// SPK
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
+
+// tables -> Kriteria
+Route::get('/tables', function () {
+    return view('tables');
+})->name('tables')->middleware('auth');
+
+// RTL -> Alternatif
+
+// alternatif weboender
+// Route::get('/alternatif', function () {
+//     return view('RTL');
+// })->name('alternatif')->middleware('auth');
+
+// Route::get('/', function () {
+//     return redirect('/dashboard');
+// })->middleware('auth');
+
+Route::get('/wallet', function () {
+    return view('wallet');
+})->name('wallet')->middleware('auth');
+
+Route::get('/profile', function () {
+    return view('account-pages.profile');
+})->name('profile')->middleware('auth');
+
+Route::get('/signin', function () {
+    return view('account-pages.signin');
+})->name('signin');
+
+Route::get('/signup', function () {
+    return view('account-pages.signup');
+})->name('signup')->middleware('guest');
+
+Route::get('/sign-up', [RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('sign-up');
+
+Route::post('/sign-up', [RegisterController::class, 'store'])
+    ->middleware('guest');
+
+Route::get('/sign-in', [LoginController::class, 'create'])
+    ->middleware('guest')
+    ->name('sign-in');
+
+Route::post('/sign-in', [LoginController::class, 'store'])
+    ->middleware('guest');
+
+Route::post('/logout', [LoginController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])
+    ->middleware('guest')
+    ->name('password.request');
+
+Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'create'])
+    ->middleware('guest')
+    ->name('password.reset');
+
+Route::post('/reset-password', [ResetPasswordController::class, 'store'])
+    ->middleware('guest');
+
+Route::get('/laravel-examples/user-profile', [ProfileController::class, 'index'])->name('users.profile')->middleware('auth');
+Route::put('/laravel-examples/user-profile/update', [ProfileController::class, 'update'])->name('users.update')->middleware('auth');
+Route::get('/laravel-examples/users-management', [UserController::class, 'index'])->name('users-management')->middleware('auth');
